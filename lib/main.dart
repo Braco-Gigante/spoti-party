30156070ca5f9c4a0d8a83e1de355c552572a7dd
Rodrigo Geurgas <rodrigogeurgas@gmail.com>
2018-08-11 19:52:59 -0300

    Spotify design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +11,85 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SpotiParty',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        buttonColor: Colors.green,
+        buttonTheme: ButtonThemeData(
+          padding: const EdgeInsets.all(12.0),
+          shape: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(32.0),
+            ),
+          ),
+        ),
+        textTheme: TextTheme(
+          display4: const TextStyle(
+              debugLabel: 'whiteMountainView display4',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white70,
+              decoration: TextDecoration.none),
+          display3: const TextStyle(
+              debugLabel: 'whiteMountainView display3',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white70,
+              decoration: TextDecoration.none),
+          display2: const TextStyle(
+              debugLabel: 'whiteMountainView display2',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white70,
+              decoration: TextDecoration.none),
+          display1: const TextStyle(
+              debugLabel: 'whiteMountainView display1',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white70,
+              decoration: TextDecoration.none),
+          headline: const TextStyle(
+              debugLabel: 'whiteMountainView headline',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white,
+              decoration: TextDecoration.none),
+          title: const TextStyle(
+              debugLabel: 'whiteMountainView title',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white,
+              decoration: TextDecoration.none),
+          subhead: const TextStyle(
+              debugLabel: 'whiteMountainView subhead',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white,
+              decoration: TextDecoration.none),
+          body2: const TextStyle(
+              debugLabel: 'whiteMountainView body2',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white,
+              decoration: TextDecoration.none),
+          body1: const TextStyle(
+              debugLabel: 'whiteMountainView body1',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white,
+              decoration: TextDecoration.none),
+          caption: const TextStyle(
+              debugLabel: 'whiteMountainView caption',
+              fontFamily: 'Roboto',
+              inherit: true,
+              color: Colors.white70,
+              decoration: TextDecoration.none),
+          button: const TextStyle(
+              debugLabel: 'whiteMountainView button',
+              fontFamily: 'Roboto',
+              fontSize: 16.0,
+              // inherit: true,
+              color: Colors.white,
+              decoration: TextDecoration.none),
+        ),
       ),
       home: MyHomePage(
         title: 'SpotiParty',
@@ -27,21 +108,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  void _create_server() {
+    int port = 1234;
+
+    HttpServer.bind(InternetAddress.loopbackIPv4, port).
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            MaterialButton(
+            RaisedButton(
               child: Center(child: Text('Host Party')),
-              onPressed: () {},
+              onPressed: () {
+                _create_server();
+              },
             ),
-            MaterialButton(
+            const SizedBox(height: 16.0),
+            RaisedButton(
               child: Center(child: Text('Join Party')),
               onPressed: () {},
             ),
